777798af0aa5fbf40fcb853b01945f152cc61849
Richer Duong <duongricher@gmail.com>
2023-12-16 23:01:07 -0600

    Add js code

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/* Show menu */
+const navMenu = document.getElementById('nav-menu'), // Get element with ID 'nav-menu' and store in navMenu
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Validate if constant exists */
+if(navToggle){  // Check if navToggle exists
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu') // Add show-menu class when clicked
+    })
+}
+
+/* Hide menu */
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* Remove menu on mobile */
+const navLink = document.querySelectorAll('.nav__link') // Selects all elements with class 'nav__link' and stores them in 'navLink'
+
+// Define function names linkAction
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu') // This hides the navigation menu
+}
+navLink.forEach(n => n.addEventListener('click', linkAction)) // Loop over each element in navLink NodeList, then the linkAction will be called on click events
