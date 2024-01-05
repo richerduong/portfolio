edfde6d2ce6b7d064a04b7474bf480d209eee2e6
Richer Duong <duongricher@gmail.com>
2024-01-04 20:29:03 -0600

    Fix js errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,28 +37,6 @@ const scrollUp = () => {
 }
 window.addEventListener('scroll', scrollUp)
 
-/* Scroll Sections Active Link */
-const sections = document.querySelectorAll('section[id]')
-
-const scrollActive = () => {
-    const scrollDown = window.scrollY
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
-        }else{
-            sectionsClass.classList.remove('active-link')
-        }
-    })
-}
-
-window.addEventListener('scroll', scrollActive)
-
 /* Dark Light Theme */
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
